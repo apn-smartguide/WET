690a39a9ff8dd763f618aac27e9bc4d851492938
Brian McIntyre <brian@alphinat.com>
2021-08-19 01:07:18 -0400

    Refactor smartguide.js loading

--- resources/js/smartguide/smartguide-wet.js
+++ resources/js/smartguide/smartguide-wet.js
@@ -1,11 +1,7 @@
 //In WET, we must fist let WET load and wait for it to fire it's wb-ready envent.
 //Then we can load smartguide.js
 wb.doc.on("wb-ready.wb", function (event) {
-	Modernizr.load( {
-		load: [
-			basePath + "default_8.5/resources/js/smartguide/smartguide.js?v8.5",
-		],
-		complete: function() {
-		}
-	} );
+	for(let smartlet in SMARTGUIDES){
+		SMARTGUIDES[smartlet].init();
+	}
 });
